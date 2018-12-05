1da1ba39dac513c9aaaec36da1156de08f95c30e
igor.vasilev@nevosoft.ru <nevosoft@CharmFarmBuildMachine.local>
2018-12-05 17:32:37 +0300

    fix: framework change files structure

--- project/common/AppsFlyerExtension.mm
+++ project/common/AppsFlyerExtension.mm
@@ -2,7 +2,7 @@
 #import <UIKit/UIKit.h>
 #import <AppsFlyerLib/AppsFlyerTracker.h>
 
-@interface AppsFlyerTrackerController : NSObject<UIApplicationDelegate>
+@interface AppsFlyerTrackerController : NSObject<UIApplicationDelegate, AppsFlyerTrackerDelegate>
 @end
 
 @implementation AppsFlyerTrackerController
