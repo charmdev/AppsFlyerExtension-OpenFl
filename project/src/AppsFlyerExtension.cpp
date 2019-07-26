b589736afab7acd0f3bca283c408db34de9031dd
Zaphod <zaphy@yandex.ru>
2019-07-26 14:05:41 +0300

    fix extension compilation for NME

---
+++ project/src/AppsFlyerExtension.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+namespace appsflyerextension 
+{
+	void StartTracking(std::string devkey, std::string appId)
+	{
+		
+	}
+	
+	void TrackEvent(std::string eventName, std::string eventData)
+	{
+		
+	}
+}
