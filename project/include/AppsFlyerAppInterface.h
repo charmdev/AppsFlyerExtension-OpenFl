5d493fffb758862325d1212640c45495215832d6
Evgeny <Evgeny>
2021-03-15 15:02:14 +0600

    update ios api

--- project/include/AppsFlyerAppInterface.h
+++ project/include/AppsFlyerAppInterface.h
@@ -3,10 +3,13 @@
 #include <string>
 
 namespace appsflyerextension {
-	void TrackAppLaunch();
-	void TrackEvent(std::string eventName, std::string eventData);
-	void Init();
-}
 
+	void TrackAppLaunch();
+
+	void TrackEvent(std::string eventName, std::string eventData);
+
+	void Init();
+	
+}
 
 #endif
--- project/src/AppsFlyerAppDelegate.mm
+++ project/src/AppsFlyerAppDelegate.mm
@@ -1,11 +1,11 @@
 #include "AppsFlyerAppInterface.h"
 #import <UIKit/UIKit.h>
-#import <AppsFlyerLib/AppsFlyerTracker.h>
+#import <AppsFlyerLib/AppsFlyerLib.h>
 
 extern "C" void returnConversionSuccess (const char* data);
 extern "C" void returnConversionError ();
 
-@interface ConversionListener : NSObject <AppsFlyerTrackerDelegate>
+@interface ConversionListener : NSObject <AppsFlyerLibDelegate>
 @end
 
 // Copied from Apple's header in case it is missing in some cases (e.g. pre-Xcode 8 builds).
@@ -19,74 +19,65 @@ static NSMutableString *resultString;
 static NSString *errorString;
 
 
-/*
--(BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *) launchOptions
+-(void)onConversionDataSuccess:(NSDictionary*) installData
 {
-    [AppsFlyerTracker sharedTracker].delegate = self;
-    NSLog(@"willFinishLaunchingWithOptions AppsFlyer");
-    return YES;
-}
-*/
+	NSLog(@"%@", installData);
+	id status = [installData objectForKey:@"af_status"];
+	if([status isEqualToString:@"Non-organic"]) {
+		id sourceID = [installData objectForKey:@"media_source"];
+		id campaign = [installData objectForKey:@"campaign"];
+		NSLog(@"This is a none organic install. Media source: %@  Campaign: %@",sourceID,campaign);
+	} else if([status isEqualToString:@"Organic"]) {
+		NSLog(@"This is an organic install.");
+	}
 
--(void)onConversionDataReceived:(NSDictionary*) installData
-{
-    NSLog(@"%@", installData);
-    id status = [installData objectForKey:@"af_status"];
-    if([status isEqualToString:@"Non-organic"]) {
-        id sourceID = [installData objectForKey:@"media_source"];
-        id campaign = [installData objectForKey:@"campaign"];
-        NSLog(@"This is a none organic install. Media source: %@  Campaign: %@",sourceID,campaign);
-    } else if([status isEqualToString:@"Organic"]) {
-        NSLog(@"This is an organic install.");
-    }
-
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSMutableString* referrerUri = [[NSMutableString alloc] init];
-        for (NSString* key in [installData allKeys]) {
-            if ([referrerUri length]>0)
-                [referrerUri appendString:@"&"];
-            [referrerUri appendFormat:@"%@=%@", key, [installData objectForKey:key]];
-        }
-        returnConversionSuccess([referrerUri UTF8String]);
-    });
+	dispatch_async(dispatch_get_main_queue(), ^{
+		NSMutableString* referrerUri = [[NSMutableString alloc] init];
+		for (NSString* key in [installData allKeys]) {
+			if ([referrerUri length]>0)
+				[referrerUri appendString:@"&"];
+			[referrerUri appendFormat:@"%@=%@", key, [installData objectForKey:key]];
+		}
+		returnConversionSuccess([referrerUri UTF8String]);
+	});
 }
 
--(void)onConversionDataRequestFailure:(NSError *) error {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        returnConversionError();
-    });
+-(void)onConversionDataFail:(NSError *) error {
+	dispatch_async(dispatch_get_main_queue(), ^{
+		returnConversionError();
+	});
 }
 
 @end
 
 namespace appsflyerextension {
 
-    void Init()
-    {
-        [AppsFlyerTracker sharedTracker].appleAppID = [[NSBundle mainBundle].infoDictionary objectForKey:@"AppsFlyerAppId"];
-        [AppsFlyerTracker sharedTracker].appsFlyerDevKey = [[NSBundle mainBundle].infoDictionary objectForKey:@"AppsFlyerDevKey"];
-        NSLog(@"appsflyerextension Init appId:%@, appKey:%@", [AppsFlyerTracker sharedTracker].appleAppID, [AppsFlyerTracker sharedTracker].appsFlyerDevKey);
+	void Init()
+	{
+		[AppsFlyerLib shared].appleAppID = [[NSBundle mainBundle].infoDictionary objectForKey:@"AppsFlyerAppId"];
+		[AppsFlyerLib shared].appsFlyerDevKey = [[NSBundle mainBundle].infoDictionary objectForKey:@"AppsFlyerDevKey"];
+		NSLog(@"appsflyerextension Init appId:%@, appKey:%@", [AppsFlyerLib shared].appleAppID, [AppsFlyerLib shared].appsFlyerDevKey);
 
-        ConversionListener *listener = [[ConversionListener alloc] init];
-        [AppsFlyerTracker sharedTracker].delegate = listener;
-        NSLog(@"appsflyerextension Init");
-    }
-    
-    void TrackAppLaunch() {
-        NSLog(@"appsflyerextension TrackAppLaunch");
-        [[AppsFlyerTracker sharedTracker] trackAppLaunch];
-    }
+		ConversionListener *listener = [[ConversionListener alloc] init];
+		[AppsFlyerLib shared].delegate = listener;
+		NSLog(@"appsflyerextension Init");
+	}
+	
+	void TrackAppLaunch() {
+		NSLog(@"appsflyerextension start");
+		[[AppsFlyerLib shared] start];
+	}
 
-    void TrackEvent(std::string eventName, std::string eventData) {
-        NSLog(@"appsflyerextension TrackEvent");
-        NSString* eName = [[NSString alloc] initWithUTF8String:eventName.c_str()];
-        NSString* jsonStr = [[NSString alloc] initWithUTF8String:eventData.c_str()];
-        NSData* data = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
-        
-        NSDictionary *responseDic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-        [[AppsFlyerTracker sharedTracker] trackEvent:eName withValues:responseDic];
-        
-        NSLog(@"%@", responseDic);
-    }
-    
+	void TrackEvent(std::string eventName, std::string eventData) {
+		NSLog(@"appsflyerextension TrackEvent");
+		NSString* eName = [[NSString alloc] initWithUTF8String:eventName.c_str()];
+		NSString* jsonStr = [[NSString alloc] initWithUTF8String:eventData.c_str()];
+		NSData* data = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+		
+		NSDictionary *responseDic = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+		[[AppsFlyerLib shared] logEvent:eName withValues:responseDic];
+		
+		NSLog(@"%@", responseDic);
+	}
+	
 }
